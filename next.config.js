b4c54f8e06a25d6635fab59b346a500c73303e44
Marcelo Andrés <mmenddozaf@gmail.com>
2023-11-07 02:24:37 -0300

    feat: next-i18next added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'sunbeam.rweb.cl',
+            port: '',
+            pathname: '/api/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
